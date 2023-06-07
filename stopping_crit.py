44272d52a7b198e06160b00d77f2d4095a63b8a2
Eddie Cueto <cueto303@gmail.com>
2023-06-07 08:50:19 +0100

    Modified early stoping to stop only after 0.50

--- stopping_crit.py
+++ stopping_crit.py
@@ -1,22 +1,37 @@
-def earlyStopping(early_stopping: list, train_acc: float, sensitivity: float=1e-9):
+import amd_sample_draw
+from time import sleep
+
+
+def earlyStopping(early_stopping: list, train_acc: float, epoch: int, sensitivity: float=1e-9):
     early_stopping.append(train_acc)
     if epoch % 4 == 0 and epoch > 0:
-        print("Value 1: {} >= {}, Value 2: {} >= {}, \
-            Value 2: {} >= {}".format(early_stopping[0], \
-            train_acc-sensitivity,early_stopping[1], \
-            train_acc-sensitivity, early_stopping[2], train_acc-sensitivity))
-        if abs(early_stopping[0]) >= train_acc-sensitivity and \
-            abs(early_stopping[1]) >= train_acc-sensitivity and \
-            abs(early_stopping[2]) >= train_acc-sensitivity:
-            return None
-        early_stopping = []
+        print("Value 1: {} > Value 2: {} > \
+            Value 3: {}".format(early_stopping[0], \
+            abs(early_stopping[1]-sensitivity), \
+            abs(early_stopping[2]-sensitivity)))
+        if train_acc > 0.5:
+            if early_stopping[0] > abs(early_stopping[1]-sensitivity) and \
+               early_stopping[1] > abs(early_stopping[2]-sensitivity):
+                print("Stopping Early")
+                return 1
+        del early_stopping[:]
+    return 0
         
 
 def energyBound(threshold: float=100000.0):
-    if gpu_sample_draw.total_watt_consumed() > threshold:
-        return None
-
+    try:
+        energy = amd_sample_draw.total_watt_consumed()
+    except Exception as e:
+        sleep(3)
+        energy = amd_sample_draw.total_watt_consumed()
+    print("Energy used: {}".format(energy))
+    if energy > threshold:
+        print("Energy bound achieved")
+        return 1
+    return 0
 
 def accuracyBound(train_acc: float, threshold: float=0.99):
     if train_acc >= threshold:
-        return None
+        print("Accuracy bound achieved")
+        return 1
+    return 0
